7823a5b2ec3cab04c4ec4ec5171123caf66657c3
Avaer Kazmer <a@modules.io>
2018-03-19 02:30:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Intrakit test module');
+const port = process.env['PORT'] || 8000;
+console.log('Hello, world 1', port);
+
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.end(`got req: ${JSON.stringify(req.url)}\n`);
+})
+  .listen(port, err => {
+    if (!err) {
+      console.log(`listening on http://127.0.0.1:${port}`);
+    } else {
+      throw err;
+    }
+  });
